2e624eac21fa5106eb2a06b6cc8572abdaf45a6c
Champlain <champlainmarius20@gmail.com>
2020-12-02 11:53:26 +0100

    inistialisation du dépot

---
+++ src/main/java/com/bixterprise/pmes/PmesApplication.java
@@ -0,0 +1,13 @@
+package com.bixterprise.pmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmesApplication.class, args);
+	}
+
+}
